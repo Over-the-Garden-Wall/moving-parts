516b37af530590b2304282a544bdef64c79f32f4
sunnymilk- <orb33@yahoo.com>
2015-10-11 23:27:14 -0400

    Starting GUI!

--- __main__.py
+++ __main__.py
@@ -5,12 +5,17 @@ Spyder Editor
 This is a temporary script file.
 """
 
+#from math import *
 from Tkinter import *
+from ttk import *
+
 
 
 
 class Application(Frame):
     """ A GUI app! """
+    #cards = []    
+    max_rows = 5;
     
     def __init__(self, master):
         #initialize the framezor
@@ -18,22 +23,63 @@ class Application(Frame):
         self.grid()
         self.button_clicks =0 #click counter!
         self.create_widgets()
+        self.cards = []
         
     def create_widgets(self):
         #make the three buttons
     
         self.button1 = Button(self)
-        self.button1["text"] = "Click me!"
-        self.button1["command"] = self.update_count        
-        self.button1.grid()
-        
-        
-    def update_count(self):
-        #increase the click count and display the new total
-        self.button_clicks += 1
-        self.button1["text"] = "Clicks: " + str(self.button_clicks)
+        self.button1["text"] = "Click me!"        
+        self.button1["command"] = self.add_card        
+        self.button1.grid() 
         
 
+        
+    def add_card(self):
+        #increase the click count and display the new total
+    
+        self.cards.append(Card("./samp_im.gif"))
+        r = self.button_clicks % 5
+        c = 1 + int(self.button_clicks/5)
+                
+        
+        self.cards[self.button_clicks].grid(row=r, column=c)
+        self.button_clicks += 1
+        
+    def nonsense_click(self):
+        
+        self.button_clicks += 1
+        self.button1["text"] = "card #" + str(self.button_clicks)
+        #self.add_card()
+        
+        
+
+class Card(Canvas):
+    #A card object
+    
+    def __init__(self, img_fn):
+        Canvas.__init__(self)
+        self.configure(width = 50)
+        self.configure(height = 50)
+        self.img = PhotoImage(file = img_fn)
+        self.create_image(25,25, image=self.img)
+        self.bind("<Button-3>", self.right_click)
+        self.bind("<Button-1>", self.left_click)
+        
+        #Canvas.__init__(self, *args, **kwargs)
+        #super(Card, self).__init__(*args, **kwargs)
+        
+    def right_click(self, event):
+        self.img = PhotoImage(file = "./samp3.gif")
+        self.create_image(25,25, image=self.img)
+        
+
+    def left_click(self, event):
+        self.img = PhotoImage(file = "./samp2.gif")
+        self.create_image(25,25, image=self.img)
+        
+        
+        
 
 
 #create window
@@ -41,10 +87,9 @@ root = Tk()
 
 #modify root window
 root.title("main window")
-root.geometry("600x400")
+root.geometry("800x800")
 
 app = Application(root)
-
 
 """
 
